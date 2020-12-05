f1873bf1df8d5ffc9ef5047fb67c29fc63c7786f
CorneVgit <corneverduin@hotmail.com>
2020-12-05 14:42:35 +0100

    day 5

---
+++ src/day5.ts
@@ -0,0 +1,49 @@
+import fs from 'fs';
+
+function main() {
+    const input = fs.readFileSync("data/input_day5.txt").toString("ascii").split("\n");
+    let highest_seat_id = 0;
+    let seat_ids: number[] = [];
+
+    for (const s of input) {
+        let row_step = 64;
+        let column_step = 4;
+        let row_ub = 127;
+        let column_ub = 7;
+
+        for (const c of s) {
+            switch (c) {
+                case "F":
+                    row_ub -= row_step;
+                    row_step /= 2;
+                    break;
+                case "B":
+                    row_step /= 2;
+                    break;
+                case "L":
+                    column_ub -= column_step;
+                    column_step /= 2;
+                    break;
+                case "R":
+                    column_step /= 2;
+                    break;
+            }
+        }
+
+        const seat_id = 8 * row_ub + column_ub;
+
+        seat_ids.push(seat_id);
+        highest_seat_id = Math.max(seat_id, highest_seat_id);
+    }
+
+    seat_ids.sort((a, b) => { return a - b; });
+    for (let index = 1; index < seat_ids.length; index++) {
+        if (seat_ids[index - 1] + 2 === seat_ids[index]) {
+            console.log(seat_ids[index - 1] + 1);
+
+        }
+
+    }
+}
+
+main();
