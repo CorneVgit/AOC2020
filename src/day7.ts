550b195b45a085cb82eb77f583ef3c065e6aa651
CorneVgit <corneverduin@hotmail.com>
2020-12-07 17:03:11 +0100

    refactor

--- src/day7.ts
+++ src/day7.ts
@@ -7,14 +7,14 @@ function main() {
 
     for (const s of input) {
         let bag = "";
-        const contain = new Map<string, number>();
-        for (const m of s.matchAll(/(?<bag>[\w]* [\w]*?)?(?: )(?:bags contain)?(?: )?(?<amount>[\d]+)? (?<contain>[\w]* [\w]*)?/g)) {
+        const contained = new Map<string, number>();
+        for (const m of s.matchAll(/(?<bag>[\w]* [\w]*?)?(?: )(?:bags contain)?(?: )?(?<amount>[\d]+)? (?<contained>[\w]* [\w]*)?/g)) {
             if (m.groups?.bag) bag = m.groups?.bag;
-            if (m.groups?.contain && m.groups?.amount) {
-                contain.set(m.groups.contain, Number(m.groups.amount));
+            if (m.groups?.contained && m.groups?.amount) {
+                contained.set(m.groups.contained, Number(m.groups.amount));
             }
         }
-        if (bag !== "") bags.set(bag, contain);
+        if (bag !== "") bags.set(bag, contained);
     }
 
     for (const bag of bags.keys()) {
@@ -29,10 +29,10 @@ function main() {
 function check_bag(bags: Map<string, Map<string, number>>, bag: string, count = 0) {
     const current_bag = bags.get(bag);
 
-    if (current_bag !== undefined) {
+    if (current_bag) {
         if (current_bag.has("shiny gold")) {
             count++;
-        } else if (current_bag.size !== 0) {
+        } else if (current_bag.size > 0) {
             for (const b of current_bag.keys()) {
                 count += check_bag(bags, b, count);
             }
@@ -45,17 +45,17 @@ function check_bag(bags: Map<string, Map<string, number>>, bag: string, count = 
 function count_bags(bags: Map<string, Map<string, number>>, bag: string, total_amount = -1) {
     const current_bag = bags.get(bag);
 
-    if (current_bag !== undefined) {
-        if (current_bag.size !== 0) {
-            for (const b of current_bag.keys()) {
-                const amount = current_bag.get(b);
-
-                if (amount !== undefined) {
-                    total_amount += amount * count_bags(bags, b, 0);
-                }
-            }
-        } else {
+    if (current_bag) {
+        if (current_bag.size === 0) {
             return 1;
+        }
+
+        for (const b of current_bag.keys()) {
+            const amount = current_bag.get(b);
+
+            if (amount) {
+                total_amount += amount * count_bags(bags, b, 0);
+            }
         }
     }
 
