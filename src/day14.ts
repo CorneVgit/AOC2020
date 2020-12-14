fdbeaa63e2f0071e0af74694e471cb1aca366093
CorneVgit <corneverduin@hotmail.com>
2020-12-14 15:26:14 +0100

    refactor

--- src/day14.ts
+++ src/day14.ts
@@ -16,11 +16,13 @@ function get_masks(input: string[]) {
 
     for (const s of input) {
         let result = /mask = (?<mask>(?:X|0|1){36})/.exec(s);
+
         if (result && result.groups?.mask) {
             mask = result.groups?.mask;
             memory_map.clear();
         } else if (mask) {
             result = /mem\[(?<location>\d+)\] = (?<value>\d+)/.exec(s);
+            
             if (result && result.groups?.location && result.groups?.value) {
                 memory_map.set(Number(result.groups?.location).toString(2).padStart(36, '0'), Number(result.groups?.value).toString(2).padStart(36, '0'));
                 masks.set(mask, new Map(memory_map));
@@ -32,7 +34,7 @@ function get_masks(input: string[]) {
 }
 
 function decode1(masks: Map<string, Map<string, string>>) {
-    const locations = new Map<string, number>();
+    const addresses = new Map<string, number>();
 
     for (const [mask, memory_map] of masks) {
         for (const m of memory_map) {
@@ -50,15 +52,15 @@ function decode1(masks: Map<string, Map<string, string>>) {
                 }
             }
 
-            locations.set(address, parseInt(v.join(""), 2));
+            addresses.set(address, parseInt(v.join(""), 2));
         }
     }
 
-    return locations;
+    return addresses;
 }
 
 function decode2(masks: Map<string, Map<string, string>>) {
-    const locations = new Map<string, number>();
+    const addresses = new Map<string, number>();
 
     for (const [mask, memory_map] of masks) {
         for (const m of memory_map) {
@@ -76,11 +78,11 @@ function decode2(masks: Map<string, Map<string, string>>) {
                 }
             }
 
-            for (const a of get_combinations(address.join(''))) locations.set(a, parseInt(v, 2));
+            for (const a of get_combinations(address.join(''))) addresses.set(a, parseInt(v, 2));
         }
     }
 
-    return locations;
+    return addresses;
 }
 
 function get_sum(locations: Map<string, number>) {
